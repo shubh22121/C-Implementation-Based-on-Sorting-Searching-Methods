3a093816e919fa1fee4faaea5579a8dfe5aec4c5
shubham <shubham22121@gmail.com>
2021-12-28 23:30:24 +0530

    queue

--- 06-queue/queue.cpp
+++ 06-queue/queue.cpp
@@ -1,109 +1,90 @@
 #include<iostream>
-using namespace std; 
-#define MAX_SIZE 101  //maximum size of the array that will store Queue. 
-
-// Creating a class named Queue.
-class Queue
+using namespace std;
+# define max 6
+int queue[max];
+int front=-1;
+int rear=-1;
+void enqueue(int ele)
 {
-private:
-	int A[MAX_SIZE];
-	int front, rear; 
-public:
-	// Constructor - set front and rear as -1. 
-	// We are assuming that for an empty Queue, both front and rear will be -1.
-	Queue()
-	{
-		front = -1; 
-		rear = -1;
-	}
+    if(front==-1 && rear==-1)
+    {
+        front=0;
+        rear=0;
+        queue[rear]=ele;
+    }
+    else if((rear+1)%max==front){
+            cout<<"Queue is overflow!!";
+    }
+    else
+    {
+        rear=(rear+1)%max;
+        queue[rear]=ele;
+    }
+}
+int dequeue()
+{
+    if((front==-1)&& (rear==-1))
+    {
 
-	// To check wheter Queue is empty or not
-	bool IsEmpty()
-	{
-		return (front == -1 && rear == -1); 
-	}
+        cout<<"\n queue is underflow..";
+    }
+    else if(front==rear)
+    {
+      cout<<"the deueued element is:"<<queue[front];
+      front--;
+      rear--;
 
-	// To check whether Queue is full or not
-	bool IsFull()
-	{
-		return (rear+1)%MAX_SIZE == front ? true : false;
-	}
+    }
+    else{
+        cout<<"the dequeued element is"<<queue[front];
+        front=(front+1)%max;
+    }
 
-	// Inserts an element in queue at rear end
-	void Enqueue(int x)
-	{
-		cout<<"Enqueuing "<<x<<" \n";
-		if(IsFull())
-		{
-			cout<<"Error: Queue is Full\n";
-			return;
-		}
-		if (IsEmpty())
-		{ 
-			front = rear = 0; 
-		}
-		else
-		{
-		    rear = (rear+1)%MAX_SIZE;
-		}
-		A[rear] = x;
-	}
+    return 0;
+}
+void display()
+{
+    if(front==-1 && rear==-1)
+    {
+        cout<<"\n queue is empty..";
+    }
+    else
+    {
+        for(int i=front;i<=rear;i++)
+        {
+            cout<<queue[i]<<" ";
+        }
+    }
 
-	// Removes an element in Queue from front end. 
-	void Dequeue()
-	{
-		cout<<"Dequeuing \n";
-		if(IsEmpty())
-		{
-			cout<<"Error: Queue is Empty\n";
-			return;
-		}
-		else if(front == rear ) 
-		{
-			rear = front = -1;
-		}
-		else
-		{
-			front = (front+1)%MAX_SIZE;
-		}
-	}
-	// Returns element at front of queue. 
-	int Front()
-	{
-		if(front == -1)
-		{
-			cout<<"Error: cannot return front from empty queue\n";
-			return -1; 
-		}
-		return A[front];
-	}
-	/* 
-	   Printing the elements in queue from front to rear. 
-	   This function is only to test the code. 
-	   This is not a standard function for Queue implementation. 
-	*/
-	void Print()
-	{
-		// Finding number of elements in queue  
-		int count = (rear+MAX_SIZE-front)%MAX_SIZE + 1;
-		cout<<"Queue       : ";
-		for(int i = 0; i <count; i++)
-		{
-			int index = (front+i) % MAX_SIZE; // Index of element while travesing circularly from front
-			cout<<A[index]<<" ";
-		}
-		cout<<"\n\n";
-	}
-};
+}
 int main()
-{
-	/*Driver Code to test the implementation
-	  Printing the elements in Queue after each Enqueue or Dequeue 
-	*/
-   Queue Q; // creating an instance of Queue. 
-   Q.Enqueue(2);  Q.Print();  
-   Q.Enqueue(4);  Q.Print();  
-   Q.Enqueue(6);  Q.Print();
-   Q.Dequeue();	  Q.Print();
-   Q.Enqueue(8);  Q.Print();
+{   
+    int choice,ele;
+    while(1)
+    {
+        cout<<"\n1.enqueue\n2.dequeue\n3.display\n4.exit\n";
+        cin>>choice;
+        switch(choice)
+        {
+            case 1:
+                cout<<"enter the element to be enqueued:";
+                cin>>ele;
+                enqueue(ele);
+                break;
+            case 2:
+                dequeue();
+                break;
+            case 3:
+                cout<<"-------------------"<<endl;
+                display();
+                cout<<"-------------------"<<endl;
+                break;
+            case 4:
+                exit(0);
+            default:
+                cout<<"\n invalid choice";
+        }
+    }
+    return 0;
+
 }
