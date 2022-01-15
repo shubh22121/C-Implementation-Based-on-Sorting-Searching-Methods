5342ecf82a9b48c82edaf08710d902389645df71
shubham <shubham22121@gmail.com>
2022-01-15 15:18:40 +0530

    binary search tree

--- 07-tree/bst.cpp
+++ 07-tree/bst.cpp
@@ -46,18 +46,23 @@ bool Search(BstNode* root,int data) {
 	}
 }
 int main() {
-	BstNode* root = NULL;  // Creating an empty tree
-	/*Code to test the logic*/
-	root = Insert(root,15);	
-	root = Insert(root,10);	
-	root = Insert(root,20);
-	root = Insert(root,25);
-	root = Insert(root,8);
-	root = Insert(root,12);
-	// Ask user to enter a number.  
-	int number;
-	cout<<"Enter number be searched\n";
-	cin>>number;
-	//If number is found, print "FOUND"
-	if(Search(root,number) == true) cout<<"Found\n";
-	else cout<<"Not Found\n";
+		BstNode* root = NULL;
+		root = Insert(root,10);
+		root = Insert(root,20);
+		root = Insert(root,30);
+		root = Insert(root,40);
+		root = Insert(root,50);
+
+		int data;
+		cout<<"Enter the data to be searched: ";
+		cin>>data;
+		if(Search(root,data)) {
+			cout<<"Element found"<<endl;
+		}
+		else {
+			cout<<"Element not found"<<endl;
+		}
+		return 0;
+
+		 
+}
